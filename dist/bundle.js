68c6d15e551858f656a70a13a76c88967cde6163
Stemmy287 <steem287@gmail.com>
2023-04-19 11:30:25 +0300

    feat: layout

--- dist/bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";function e(e){e.preventDefault();var t,u,n=document.querySelector("input[name='firstnumber']"),r=document.querySelector("input[name='secondnumber']"),o=(t=Number(n.value),u=Number(r.value),t-u),m=document.querySelector("p");m&&(m.textContent=o.toString())}var t;null==(t=document.querySelector("form"))||t.addEventListener("submit",e)})();
-//# sourceMappingURL=bundle.js.map
